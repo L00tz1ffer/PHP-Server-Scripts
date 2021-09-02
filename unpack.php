90e34eed7cf647dbdc5d956e70d2a38af992b435
L00tz1ffer <88904868+L00tz1ffer@users.noreply.github.com>
2021-09-02 23:10:29 +0200

    Update unpack.php

--- unpack.php
+++ unpack.php
@@ -2,7 +2,7 @@
  
 // Path to Zip File 
 // (in this case its extracting a file in the same Dir with the name joomla.zip)
-$file = joomla.zip; 
+$file = "joomla.zip"; 
 //instance a ZipArchive Class
 $zip = new ZipArchive;
  
@@ -10,13 +10,15 @@ $zip = new ZipArchive;
 //NOTE: give the correct path. 
 //In this example zip file is in the same folder
 $zipped = $zip->open($file);
- 
+  
 // get the absolute path to $file, where the files has to be unzipped
 $path = pathinfo(realpath($file), PATHINFO_DIRNAME);
+$path = $path."/../";
+echo "Path: ".$path."<br>"; 
  
 //check if it is actually a Zip file
 if ($zipped) {
- 
+  
 //if yes then extract it to the said folder
   $extract = $zip->extractTo($path);
  
@@ -24,6 +26,7 @@ if ($zipped) {
   if($extract){
     echo "Your file extracted to $folder_name";
   } else {
+	echo $extract;
     echo "your file not extracted";
   }
  
